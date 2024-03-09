2d8a52f4aeee0061517d1791795b92c5da23e391
Emotion <111930499+Emotion06@users.noreply.github.com>
2024-03-09 13:18:39 +0100

    Create version.lua

---
+++ version.lua
@@ -0,0 +1,27 @@
+local CurrentIP = "None"
+local rs = GetCurrentResourceName()
+local version = 1
+
+Citizen.CreateThread(function()
+    PerformHttpRequest("https://raw.githubusercontent.com/Spooni-Development/spooni_updates/main/scripts/spooni_spooner.json", function(err, text, headers)
+        local text = json.decode(text)
+        Wait(5000)        
+        if tonumber(version) == tonumber(text.version) then
+            print("^2VERSION: ^4"..rs.."^2 IS UP TO DATE!^0")
+        elseif tonumber(version) ~= tonumber(text.version) then 
+            print("^8VERSION: ^4"..rs.."^8 THERE IS A NEW VERSION AVAILABLE!^0") 
+        end
+        CurrentIP = text
+    end, 'GET')
+end)
+
+-- ^0 = White
+-- ^1 = Red
+-- ^2 = Green
+-- ^3 = Yellow
+-- ^4 = Blue
+-- ^5 = Light Blue
+-- ^6 = Purple
+-- ^7 = White
+-- ^8 = Dark Red
+-- ^9 = Dark Blue
