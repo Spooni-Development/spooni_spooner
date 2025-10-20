d45375f5dfbf7ef8c71b120f53745ecacfc43d16
Emotion06 <tim@it-secrets.de>
2025-10-20 17:38:07 +0200

    Refactor version check logic and improve error handling in update check

--- server/version.lua
+++ server/version.lua
@@ -1,19 +1,46 @@
 local resourcename = GetCurrentResourceName()
 local version = GetResourceMetadata(resourcename, "version", 0)
+local isMapping = false
+local escrow = false
 
 Citizen.CreateThread(function()
-    PerformHttpRequest("https://raw.githubusercontent.com/Spooni-Development/spooni_updates/main/scripts/spooni_spooner.json", function(err, text, headers)
-        local text = json.decode(text)
-        Wait(5000)
-        if tonumber(version) == tonumber(text.version) then
-            print("^2VERSION: ^4" .. resourcename .. "^2 IS UP TO DATE!^0")
-        elseif tonumber(version) ~= tonumber(text.version) then
-            print("^8VERSION: ^4" .. resourcename .. "^8 THERE IS A NEW VERSION AVAILABLE!^0")
-            print("^8DOWNLOAD THE UPDATE HERE: ^4https://github.com/Spooni-Development/spooni_spooner^0")
+    Wait(5000)
+    
+    PerformHttpRequest('https://raw.githubusercontent.com/Spooni-Development/spooni_updates/refs/heads/main/scripts/spooni_spooner.json', function(err, text, headers)
+        if err ~= 200 or not text then
+            print('^8' .. resourcename .. ' ^3Unable to check for updates^0')
+            return
+        end
+        
+        local success, data = pcall(json.decode, text)
+        if not success or not data then
+            print('^8' .. resourcename .. ' ^3Error parsing version data^0')
+            return
+        end
+        
+        if tonumber(version) == tonumber(data.version) then
+            if isMapping == true then
+                print('^3' .. resourcename .. ' ^2is up to date!^0')
+            else
+                print('^4' .. resourcename .. ' ^2is up to date!^0')
+            end
+        else
+            if isMapping == true then
+                print('^3' .. resourcename .. ' ^8VERSION MISMATCH!^0')
+            else
+                print('^4' .. resourcename .. ' ^8VERSION MISMATCH!^0')
+            end
+            print('^8Current: ^3' .. version .. '^8 ^3|^8 Latest: ^3' .. data.version .. '^0')
+            if escrow == true then
+                print('^8Download: ^3https://keymaster.fivem.net^0')
+            else
+                print('^8Download: ^3https://github.com/Spooni-Development/spooni_spooner^0')
+            end
         end
     end, 'GET')
 end)
 
+-- Color Codes Reference:
 -- ^0 = White
 -- ^1 = Red
 -- ^2 = Green
@@ -24,3 +51,4 @@ end)
 -- ^7 = White
 -- ^8 = Dark Red
 -- ^9 = Dark Blue
+
